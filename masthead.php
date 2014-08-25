bab9833348fa9ae0d869058780cca242e8066d55
storynory <hugh@storynory.com>
2014-08-26 00:01:23 +0100

    changed style of text adds

--- masthead.php
+++ masthead.php
@@ -39,15 +39,15 @@
 		</div>
 	</nav>					
 	<aside class=" add add space space--top note-tiny clear">
-		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-		<!-- respond -->
-		<ins class="adsbygoogle"
-		style="display:block"
-		data-ad-client="ca-pub-3254202940297578"
-		data-ad-slot="3876728706"
-		data-ad-format="auto"></ins>
-		<script>
-		(adsbygoogle = window.adsbygoogle || []).push({});
-		</script>
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- story5 - cat -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3254202940297578"
+     data-ad-slot="2665519509"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 	</aside>	
 </header>
